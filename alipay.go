cc154b39d826c61628013424ae75129d251f39c5
Centny <centny@gmail.com>
2016-08-08 13:01:24 +0800

    update encode.

--- alipay.go
+++ alipay.go
@@ -58,8 +58,8 @@ func (c *Client) CreateUrl(utype, notify_url, return_url, out_trade_no, subject,
 		var sign, _ = c.Web.ShaSign(data)
 		vals.Add("sign_type", "RSA")
 		vals.Add("sign", url.QueryEscape(sign))
-		// data, _ = url.QueryUnescape(vals.Encode())
-		return vals.Encode()
+		data, _ = url.QueryUnescape(vals.Encode())
+		return data
 	default:
 		var sign = c.Web.Md5Sign(data)
 		vals.Add("sign_type", "MD5")
